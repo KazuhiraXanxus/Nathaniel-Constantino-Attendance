fbf67b7def834bef9bccd0ec0504fbcfbaf4114d
KazuhiraXanxus <132035195+KazuhiraXanxus@users.noreply.github.com>
2023-05-29 17:14:35 +0800

    Main.java

--- May26Attendance/Main.java
+++ May26Attendance/Main.java
@@ -19,6 +19,30 @@ public class Main {
 		car.Price();  // Call the Price() method from the Car class
 		vehicle.Stop();  // Call the Stop() method from the Vehicles class
 		car.Stop();  // Call the Stop() method from the Car class
+		
+		vehicle.model();  // Call the model() method from the Vehicles class
+		boat.model();  // Call the model() method from the Boat class
+		vehicle.run();  // Call the run() method from the Vehicles class
+		boat.run();  // Call the run() method from the Boat class
+		vehicle.Tires();  // Call the Tires() method from the Vehicles class
+		boat.Tires();  // Call the Tires() method from the Boat class
+		vehicle.Color();  // Call the Color() method from the Vehicles class
+		boat.Color();  // Call the Color() method from the Boat class
+		vehicle.Speed();  // Call the Speed() method from the Vehicles class
+		boat.Speed();  // Call the Speed() method from the Boat class
+		vehicle.Price();  // Call the Price() method from the Vehicles class
+		boat.Price();  // Call the Price() method from the Boat class
+		vehicle.breaker();  // Call the breaker() method from the Vehicles class
+		
+		vehicle.model();  // Call the model() method from the Vehicles class
+		plane.model();  // Call the model() method from the Plane class
+		vehicle.run();  // Call the run() method from the Vehicles class
+		plane.run();  // Call the run() method from the Plane class
+		vehicle.Tires();  // Call the Tires() method from the Vehicles class
+		plane.Tires();  // Call the Tires() method from the Plane class
+		vehicle.Color();  // Call the Color() method from the Vehicles class
+		plane.Color();  // Call the Color() method from the Plane class
+		vehicle.Speed();  // Call the Speed() method from the Vehicles class
 	}
 
 }
