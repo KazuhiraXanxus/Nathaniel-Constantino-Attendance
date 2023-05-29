1f938a80d9913d86849b88b81adf56c6d5eb35ad
KazuhiraXanxus <132035195+KazuhiraXanxus@users.noreply.github.com>
2023-05-29 13:22:06 +0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,34 @@
+
+public class Car extends Vehicles {
+
+	@Override
+	void model() {
+		System.out.println("Toyota-VIOS");
+	}
+	
+		void run() {
+			System.out.println("stepping on the pedal");
+
+	}
+		void Tires() {
+			System.out.println("four wheels");
+
+	}
+		void Color() {
+			System.out.println("It has Jade, Black, White and Red");
+			
+	}
+		void Speed() {
+			System.out.println("0-60 is 11 secons. Top speed is 170 kmph");
+
+	}
+		void Price() {
+			System.out.println("1,005,000");
+
+	}
+		void Stop() {
+			System.out.println("hit the brakes at nag milly rock");
+
+	}
+}
+
---
+++ Vehicles.java
@@ -0,0 +1,32 @@
+
+public class Vehicles {
+
+	void model() {
+		System.out.print("The model is: ");
+	}
+	
+		void run() {
+			System.out.print("This vehicle ran ");
+
+	}
+		void Tires() {
+			System.out.print("It Consists of ");
+
+	}
+		void Color() {
+			System.out.print("It has multiple colors ");
+			
+	}
+		void Speed() {
+			System.out.print("0-60 1 sec ");
+
+	}
+		void Price() {
+			System.out.print("This costs PHP ");
+
+	}
+		void Stop() {
+			System.out.print("Stopped and ");
+
+	}
+}
